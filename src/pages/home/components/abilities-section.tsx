ee165483fd2adba1d734fe6b09d8129c835faa41
Joao Castillo <bcastillo-2022474@kinal.edu.gt>
2025-02-16 12:56:19 -0600

    feat: updating portfolio, is MEH, but good enough for now

--- src/pages/home/components/abilities-section.tsx
+++ src/pages/home/components/abilities-section.tsx
@@ -25,6 +25,15 @@ export function HabilitiesSection() {
         <span className="text-gray-700">#</span>
         <span>Habilidades</span>
       </a>
+      <p>
+        Cuento con experiencia en desarrollo frontend y backend, trabajando con tecnologías como Java, Java Swing,
+        JavaFX, JSPs, SQL, JavaScript, React, Express y Flutter. Me especializo en la creación de interfaces funcionales
+        y responsivas, optimización de bases de datos y resolución de problemas en SQL. He trabajado en la conversión de
+        mockups a UI interactivas y en el desarrollo de soluciones eficientes y escalables. Además, tengo experiencia
+        laboral como desarrollador junior y freelancer, lo que me ha permitido fortalecer mis habilidades y adaptarme a
+        diferentes proyectos y entornos de trabajo. Siempre estoy en búsqueda de aprender nuevas tecnologías y mejorar
+        mis capacidades técnicas.
+      </p>
       <ul className="flex gap-3 flex-wrap justify-center py-5">
         {logos.map((icon) => {
           return <i className="text-4xl">{icon}</i>;
